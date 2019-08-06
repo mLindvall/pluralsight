415d13ee8fac5ad77fe60e4bc6ee52abac78c92c
mlin014 <michael.a.lindvall@savealot.com>
2019-08-06 12:20:45 -0500

    example class

---
+++ reflectioncourse/src/main/java/com/michaellindvall/pluralsight/ReflectionExample.java
@@ -0,0 +1,17 @@
+/**
+ * (c) Michael Lindvall
+ *
+ * @author mlindvall
+ * ReflectionExample.java
+ * Aug 06, 2019
+ */
+package com.michaellindvall.pluralsight;
+
+/**
+ * @author mlindvall
+ */
+public class ReflectionExample {
+    public static void main(String[] args) {
+        System.out.println("HI");
+    }
+}
