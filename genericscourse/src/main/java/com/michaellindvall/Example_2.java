9c734a5af332975910196c50c36391fb1b5cfbfb
mlindvall <michael.lindvall@gmail.com>
2019-09-30 12:50:10 -0500

    added generics to min method

--- genericscourse/src/main/java/com/michaellindvall/Example_2.java
+++ genericscourse/src/main/java/com/michaellindvall/Example_2.java
@@ -1,6 +1,7 @@
 package com.michaellindvall;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
@@ -18,18 +19,23 @@ public class Example_2 {
         madMen.add(peggyOlson);
         madMen.add(bertCooper);
 
-        final Object yougestCastMember = min(madMen, new AgeComparator());
+        final Person yougestCastMember = min(madMen, new AgeComparator());
         System.out.println(yougestCastMember);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.addAll(Arrays.asList(1,3,4,5));
+
+        System.out.println(min(numbers, Integer::compare));
     }
 
-    public static Object min(List values, Comparator comparator) {
+    public static <T> T min(List<T> values, Comparator<T> comparator) {
         if (values.isEmpty()) {
             throw new IllegalArgumentException("List is empty, cannot find minimum");
         }
 
-        Object lowestElement = values.get(0);
+        T lowestElement = values.get(0);
         for (int i = 1; i < values.size(); i++) {
-            final Object element = values.get(i);
+            final T element = values.get(i);
             if (comparator.compare(element, lowestElement) < 0) {
                 lowestElement = element;
             }
