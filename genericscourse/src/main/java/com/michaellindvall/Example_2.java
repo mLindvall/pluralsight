d6f0337c384e06683de525c3737996164f3b2311
mlindvall <michael.lindvall@gmail.com>
2019-09-30 12:27:20 -0500

    before adding generics to method

--- genericscourse/src/main/java/com/michaellindvall/Example_2.java
+++ genericscourse/src/main/java/com/michaellindvall/Example_2.java
@@ -1,8 +1,40 @@
 package com.michaellindvall;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import com.michaellindvall.before.AgeComparator;
+import com.michaellindvall.models.Person;
+
 public class Example_2 {
 
     public static void main(String... args) {
+        Person donDraper = new Person("Don Draper", 89);
+        Person peggyOlson = new Person("Peggy Olson", 65);
+        Person bertCooper = new Person("Bert Cooper", 100);
+        List<Person> madMen = new ArrayList<>();
+        madMen.add(donDraper);
+        madMen.add(peggyOlson);
+        madMen.add(bertCooper);
 
+        final Object yougestCastMember = min(madMen, new AgeComparator());
+        System.out.println(yougestCastMember);
+    }
+
+    public static Object min(List values, Comparator comparator) {
+        if (values.isEmpty()) {
+            throw new IllegalArgumentException("List is empty, cannot find minimum");
+        }
+
+        Object lowestElement = values.get(0);
+        for (int i = 1; i < values.size(); i++) {
+            final Object element = values.get(i);
+            if (comparator.compare(element, lowestElement) < 0) {
+                lowestElement = element;
+            }
+        }
+
+        return lowestElement;
     }
 }
