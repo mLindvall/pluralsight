91aad184cefd32214b0e1cb8d2bd7e83c9eda7a0
mlindvall <michael.lindvall@gmail.com>
2020-01-17 12:07:47 -0600

    finished seaction on map and weakmap

--- rapid-es6-training/arrays_collections_4.js
+++ rapid-es6-training/arrays_collections_4.js
@@ -2,3 +2,42 @@
 'use strict';
 
 // Map and WeakMaps
+
+let employee1 = {name: 'Jake'};
+let employee2 = {name: 'Janet'};
+
+let employees = new Map();
+employees.set(employee1, 'ABC');
+employees.set(employee2, '123');
+
+console.log(employees.get(employee1)); // ABC
+console.log(employees.size); // 2
+
+employees.delete(employee2);
+console.log(employees.size); // 1
+
+employees.clear();
+console.log(employees.size); // 0
+
+let arr = [
+    [employee1, 'ABC'],
+    [employee2, '123'],
+];
+
+employees = new Map(arr);
+console.log(employees.size); // 2
+console.log(employees.has(employee2)); // true
+
+let list = [...employees.values()];
+console.log(list); // [ 'ABC', '123' ]
+
+list = [...employees.entries()];
+console.log(list[0][1]); // ABC
+console.log(list[0]); // [ { name: 'Jake' }, 'ABC' ]
+
+// WeakMaps
+employees = new WeakMap(arr);
+
+employee1 = null;
+
+console.log(employees.size); // undefined // we can't access size on WeakMap
